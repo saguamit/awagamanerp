ce1723a74e6332593becfbfea99c45daf6b77611
saguamit <saguamit98@gmail.com>
2026-06-24 14:20:26 +0530

    Switch release defaults to cloud mode for v1.0.21

--- Awagaman ERP/Properties/AssemblyInfo.cs
+++ Awagaman ERP/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.20.0")]
-[assembly: AssemblyFileVersion("1.0.20.0")]
+[assembly: AssemblyVersion("1.0.21.0")]
+[assembly: AssemblyFileVersion("1.0.21.0")]
